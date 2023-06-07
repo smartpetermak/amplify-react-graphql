56876b67adfa127817246dbbcfadd2338ecb29c4
petermak <peter@levarti.com>
2023-06-07 22:47:59 +1000

    first edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload again.
+	  <br/>
+	  Edit 2
         </p>
         <a
           className="App-link"
